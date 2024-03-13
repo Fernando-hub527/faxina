5f355f02694c562c52b43515be9c33fe56917514
Fernando-hub527 <fernandocoelhosaraivanando@gmail.com>
2024-03-12 23:05:23 -0300

    Atualizar forma de gerenciar sessões
    Fazer uso do express-session para gerenciar sessão

--- api/server/src/app/startApp.ts
+++ api/server/src/app/startApp.ts
@@ -2,7 +2,9 @@ import express, { json } from "express"
 import cors from "cors"
 import { setRouts } from "./routs"
 import { createTables } from "../../script/startDatabase"
-
+import RedisStore from "connect-redis"
+import {createClient} from "redis"
+import session from "express-session"
 export class StartApp{
     app: express.Application
   
@@ -10,29 +12,32 @@ export class StartApp{
       this.app = express()
     }
 
-    async setUpDatabase(resetDatabase: boolean){
-      await createTables(resetDatabase)
-    }
-  
-    start (port: number) {
+    start(port: number) {
       
-      this.setMiddleares()
+      this.setMiddleares(this.setSessionControl())
       this.app.listen(port)
       return this.app
     }
   
     async startTeste () {
-      await this.setUpDatabase(true)
-      this.setMiddleares()
+      await createTables(true)
+      this.setMiddleares(this.setSessionControl())
       return this.app
     }
+
+    setSessionControl(){
+      let redisClient = createClient()
+      redisClient.connect().catch(console.error)
+      return new RedisStore({client: redisClient, prefix: "faxina:"})
+    }
   
-    private async setMiddleares () {
+    private async setMiddleares (store: RedisStore) {
       this.app.use((req, res, next) => {
         res.header('Access-Control-Allow-Origin', '*')
         res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
         next()
       })
+      this.app.use(session({store: store, resave: false, saveUninitialized: false, secret: "keyboard cat"}))
   
       this.app.use(cors())
       this.app.use(json())
--- api/server/src/utils/session/session.ts
+++ api/server/src/utils/session/session.ts
@@ -3,21 +3,21 @@ import { setRedis } from "../../../infra/memoryDatabase/redisSettings";
 import { SessionUserDTO } from "../../modules/user/dto/SessionUserDTO";
 import { Hash } from "crypto";
 
-export async function validSession(req: Request, res: Response, next: any){
-    let token = req.headers.authorization
-    if (!token) return res.status(401).json({ auth: false, message: 'No token provided.' })
+// export async function validSession(req: Request, res: Response, next: any){
+//     let token = req.headers.authorization
+//     if (!token) return res.status(401).json({ auth: false, message: 'No token provided.' })
 
-    const value = await (await setRedis()).get(token)
-    if(!value) return res.status(401).json({ auth: false, message: 'No active session.' })
-    req.user = value
-    next()
-}
+//     const value = await (await setRedis()).get(token)
+//     if(!value) return res.status(401).json({ auth: false, message: 'No active session.' })
+//     req.user = value
+//     next()
+// }
 
-export async function createSession(user: SessionUserDTO){
-    const client = await setRedis()
-    client.set()
-}
+// export async function createSession(user: SessionUserDTO){
+//     const client = await setRedis()
+//     client.set()
+// }
 
-function factoryToken(){
-    Hash.
-}
+// function factoryToken(){
+//     Hash.
+// }
